41274376672002e180fbe9ad36c2b68e8fe96f9d
baloerrr <akbaradityah444@gmail.com>
2025-06-27 01:53:10 +0700

    feat: add custom api response

---
+++ app/Helpers/ApiResponse.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Helpers;
+
+class ApiResponse {
+
+    public static function success($data = null, string $message = 'Success', int $code = 200) {
+        return response()->json([
+            'status' => true,
+            'message' => $message,
+            'data' => $data
+        ], $code);
+    }
+
+    public static function error(string $message = 'Error', int $code = 500, $errors = null)
+    {
+        return response()->json([
+            'status'  => 'error',
+            'message' => $message,
+            'errors'  => $errors,
+        ], $code);
+    }
+
+}
