2f29937b898bf8d5a487e4a35d684d7c57100bdc
baloerrr <akbaradityah444@gmail.com>
2025-06-27 20:21:45 +0700

    feat: add auth route

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\AuthController;
 use App\Http\Controllers\Api\User\UserController;
+use App\Http\Middleware\EnsureJwtAccessToken;
+use App\Http\Middleware\JwtFromCookieMiddleware;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,7 +11,12 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::prefix('users')->group(function () {
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/refresh', [AuthController::class, 'refreshToken']);
+
+
+Route::prefix('users')->middleware([JwtFromCookieMiddleware::class, 'role:admin'])->group(function () {
     Route::get('/', [UserController::class, 'index']);
     Route::get('/{id}', [UserController::class, 'show']);
     Route::post('/', [UserController::class, 'store']);
