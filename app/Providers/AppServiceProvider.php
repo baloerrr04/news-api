5d3ac8c30bc548acbbafeb3e77eb9b395f7dfcf9
baloerrr <akbaradityah444@gmail.com>
2025-06-27 20:19:55 +0700

    feat: add auth binding

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,12 @@
 
 namespace App\Providers;
 
+use App\Repositories\Auth\AuthRepository;
+use App\Repositories\Auth\AuthRepositoryInterface;
 use App\Repositories\User\UserRepository;
 use App\Repositories\User\UserRepositoryInterface;
+use App\Service\Auth\AuthService;
+use App\Service\Auth\AuthServiceInterface;
 use App\Service\User\UserService;
 use App\Service\User\UserServiceInterface;
 use Illuminate\Support\ServiceProvider;
@@ -17,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
     {
         $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
         $this->app->bind(UserServiceInterface::class, UserService::class);
+        $this->app->bind(AuthRepositoryInterface::class, AuthRepository::class);
+        $this->app->bind(AuthServiceInterface::class, AuthService::class);
     }
 
     /**
